28ea1c4019f2c753ad549c12623d3d7b5611db1b
Clement Escoffier <clement.escoffier@gmail.com>
2016-08-29 13:45:04 +0200

    Initial version of the Vert.x Maven Starter

---
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -0,0 +1,14 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    vertx.createHttpServer()
+        .requestHandler(req -> req.response().end("Hello Clement!"))
+        .listen(8080);
+  }
+
+}
---
+++ src/test/java/io/vertx/starter/MainVerticleTest.java
@@ -0,0 +1,40 @@
+package io.vertx.starter;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class MainVerticleTest {
+
+  private Vertx vertx;
+
+  @Before
+  public void setUp(TestContext tc) {
+    vertx = Vertx.vertx();
+    vertx.deployVerticle(MainVerticle.class.getName(), tc.asyncAssertSuccess());
+  }
+
+  @After
+  public void tearDown(TestContext tc) {
+    vertx.close(tc.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testThatTheServerIsStarted(TestContext tc) {
+    Async async = tc.async();
+    vertx.createHttpClient().getNow(8080, "localhost", "/", response -> {
+      tc.assertEquals(response.statusCode(), 200);
+      response.bodyHandler(body -> {
+        tc.assertTrue(body.length() > 0);
+        async.complete();
+      });
+    });
+  }
+
+}
